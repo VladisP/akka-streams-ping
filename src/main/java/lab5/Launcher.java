5b7b7c711f07feb84a577dc0bb7023bdee8e02a2
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-15 00:16:25 +0300

    Scripted auto-commit on change (2019-11-15 00:16:24) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -1,8 +1,12 @@
 package lab5;
 
+import akka.actor.ActorSystem;
+
 public class Launcher {
 
+    private static final String ACTOR_SYSTEM_NAME = "ping"
+
     public static void main(String[] args) {
-        
+        ActorSystem system = ActorSystem.create();
     }
 }
