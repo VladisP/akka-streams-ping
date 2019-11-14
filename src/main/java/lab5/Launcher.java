16077e5dd0381389b7592a7b1e2b5e8ae1c48656
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-15 00:22:27 +0300

    Scripted auto-commit on change (2019-11-15 00:22:27) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -1,12 +1,35 @@
 package lab5;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class Launcher {
 
-    private static final String ACTOR_SYSTEM_NAME = "ping"
+    private static final String ACTOR_SYSTEM_NAME = "ping";
+    private static final String HOST_NAME = "localhost";
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create();
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = null; //TODO: add later...
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST_NAME, PORT),
+                materializer
+        );
+        System.out.println("Server online at " + HOST_NAME + ":" + PORT);
+        System.out.println("Press RETURN to stop...");
+        System.in.read();
     }
 }
