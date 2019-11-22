f77126ad8709d9484d5b47f8683b4431357fc3ab
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 00:59:05 +0300

    Scripted auto-commit on change (2019-11-23 00:59:05) by gitwatch.sh

--- src/main/java/lab5/actors/CacheActor.java
+++ src/main/java/lab5/actors/CacheActor.java
@@ -16,7 +16,7 @@ public class CacheActor extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(PingRequest.class, (pingRequest) -> {
                     Long result = cache.getOrDefault(pingRequest.getTestUrl(), -1L);
-                    
+                    sender().tell();
                 })
                 .match()
                 .build();
