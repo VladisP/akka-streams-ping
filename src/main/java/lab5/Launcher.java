bba785ceb38ab60cbca8ff57296b6df68e171973
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 00:28:58 +0300

    Scripted auto-commit on change (2019-11-23 00:28:58) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -25,7 +25,7 @@ public class Launcher {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> httpFlow = Flow.of(HttpRequest.class).map((request) -> {
             //распарсить
-            request.getUri().query().
+            request.getUri().query().getOrElse()
         })
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 httpFlow,
