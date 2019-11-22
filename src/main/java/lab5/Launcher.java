4a7ddc816a20034905e5ab60533a324f00dcc6eb
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 01:34:02 +0300

    Scripted auto-commit on change (2019-11-23 01:34:02) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -33,7 +33,7 @@ public class Launcher {
     private static final int PARALLELISM = 6;
     private static final Duration TIMEOUT_MILLIS = Duration.ofMillis(3000);
 
-    private static 
+    private static CompletionStage<>
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
