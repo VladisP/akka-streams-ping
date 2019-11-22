21f75fdcbdecda64cbcbe8721b6ddcbfa53a4665
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 01:02:30 +0300

    Scripted auto-commit on change (2019-11-23 01:02:30) by gitwatch.sh

--- src/main/java/lab5/messages/PingResult.java
+++ src/main/java/lab5/messages/PingResult.java
@@ -3,5 +3,18 @@ package lab5.messages;
 public class PingResult {
 
     private String testUrl;
-    private Long 
+    private Long averageResponseTime;
+
+    public PingResult(String testUrl, Long averageResponseTime) {
+        this.testUrl = testUrl;
+        this.averageResponseTime = averageResponseTime;
+    }
+
+    public String getTestUrl() {
+        return testUrl;
+    }
+
+    public Long getAverageResponseTime() {
+        return averageResponseTime;
+    }
 }
