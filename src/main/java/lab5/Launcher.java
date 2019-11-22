da9c2fd30a2eb3567bcc7e2e19b880c99ccf54cf
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 01:54:29 +0300

    Scripted auto-commit on change (2019-11-23 01:54:29) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -44,7 +44,7 @@ public class Launcher {
     }
 
     private static Sink<PingRequest, CompletionStage<Long>> pingSink() {
-        
+
     }
 
     public static void main(String[] args) throws IOException {
