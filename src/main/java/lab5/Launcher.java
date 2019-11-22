0fc930abc560895934d47db1399f218f627c6769
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 01:28:50 +0300

    Scripted auto-commit on change (2019-11-23 01:28:50) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -51,7 +51,9 @@ public class Launcher {
             return new PingRequest(testUrl, count);
         })
                 .mapAsync(PARALLELISM, (pingRequest) -> Patterns.ask(cacheActor, pingRequest, TIMEOUT_MILLIS)
-                .thenCompose());
+                .thenCompose((result) -> {
+                    
+                }));
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 httpFlow,
                 ConnectHttp.toHost(HOST_NAME, PORT),
