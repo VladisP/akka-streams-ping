3af88abf1d7fe3bc1b653730cdc4fe876cdc7171
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 00:49:59 +0300

    Scripted auto-commit on change (2019-11-23 00:49:59) by gitwatch.sh

--- src/main/java/lab5/actors/CacheActor.java
+++ src/main/java/lab5/actors/CacheActor.java
@@ -2,7 +2,11 @@ package lab5.actors;
 
 import akka.actor.AbstractActor;
 
+import java.util.Map;
+
 public class CacheActor extends AbstractActor {
+
+    private Map<String, Long> cache
 
     @Override
     public Receive createReceive() {
