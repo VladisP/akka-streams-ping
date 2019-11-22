b09f52b9bc9978b0e0b5415eb15962678c2dddd0
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 01:46:29 +0300

    Scripted auto-commit on change (2019-11-23 01:46:29) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -42,7 +42,7 @@ public class Launcher {
                 .run(materializer);
     }
 
-    private static CompletionStage<>
+    private static Sink<>
 
     public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
