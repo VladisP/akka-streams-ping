8cc4940b1e5b94f08c040b4ef835c0149ec60a89
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 11:07:16 +0300

    Scripted auto-commit on change (2019-11-23 11:07:16) by gitwatch.sh

--- src/main/java/lab5/app/PingServer.java
+++ src/main/java/lab5/app/PingServer.java
@@ -13,5 +13,5 @@ public class PingServer {
     private static final Duration TIMEOUT_MILLIS = Duration.ofMillis(3000);
     private static final long NANO_TO_MS_FACTOR = 1_000_000L;
 
-    private static AsyncHttpClient httpClient = Dsl.asyncHttpClient();
+    private AsyncHttpClient httpClient = Dsl.asyncHttpClient();
 }
