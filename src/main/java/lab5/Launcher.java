f4c4a219e69355d046afb43910266768583bc8e4
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-23 10:56:14 +0300

    Scripted auto-commit on change (2019-11-23 10:56:14) by gitwatch.sh

--- src/main/java/lab5/Launcher.java
+++ src/main/java/lab5/Launcher.java
@@ -7,9 +7,7 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.Query;
+import akka.http.javadsl.model.*;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
@@ -98,7 +96,7 @@ public class Launcher {
                         }))
                 .map((result) -> {
                     cacheActor.tell(result, ActorRef.noSender());
-                    return HttpResponse.create().withStatus(StatusCo)
+                    return HttpResponse.create().withStatus(StatusCodes.ok)
                 });
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 httpFlow,
